0a54cffaf12beea797ae65d8b0ef2e477ff3b2f0
katherineoung <katherine.oung@gmail.com>
2025-10-17 23:35:23 -0500

    Configure Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/kath-portfolio/',
+  base: '/kath-portfolio/', // required for GitHub Pages subpath
 })
